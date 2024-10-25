f69793eff694aa21cf91e9758b4a1836e2f7712e
mozchou <leminhchau5112@gmail.com>
2024-10-25 21:57:39 +0700

    add image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './css/style.css';
 import './css/dashboard.css';
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import SideBar from "./scenes/layout/sidebar";
 import Navbar from "./scenes/layout/navbar";
 import { ColorModeContext, useMode } from "./theme";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const ToggledContext = createContext(null);
+
 function App() {
   const [theme, colorMode] = useMode();
   const [toggled, setToggled] = useState(false);
   const values = { toggled, setToggled };
+
+  // Hiển thị thông báo khi đăng nhập thành công
+  useEffect(() => {
+    toast.success("Đăng nhập thành công!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }, []); // useEffect với dependency rỗng sẽ chỉ chạy một lần khi component này render
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
@@ -23,8 +41,8 @@ function App() {
                 overflowY: "auto",
                 flex: 1,
                 maxWidth: "100%",
-                marginLeft: '3%',   // Cách lề trái 3%
-                marginRight: '3%',  // Cách lề phải 3%
+                marginLeft: '3%',
+                marginRight: '3%',
               }}
             >
               <Navbar />
@@ -33,11 +51,11 @@ function App() {
               </Box>
             </Box>
           </Box>
+          <ToastContainer /> {/* Đặt ToastContainer ở đây để quản lý tất cả các thông báo */}
         </ToggledContext.Provider>
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
-
 }
 
 export default App;
