a5fe3187b289923b6c5a38b5088d137931c6b296
mozchou <leminhchau5112@gmail.com>
2024-10-28 16:27:27 +0700

    update screen login

--- src/scenes/login/index.jsx
+++ src/scenes/login/index.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography, IconButton, InputAdornment } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import logo from "../../assets/images/logo_t286.png";
+import background from "../../assets/images/background.jpg"; // Đảm bảo đường dẫn chính xác
 
 const Login = ({ setAuthenticated }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
 
   const handleLogin = () => {
-    // Simulate authentication
     if (
       (username === 'user' && password === '1') ||
       (username === 'tbtc1' && password === '1') ||
@@ -25,6 +30,12 @@ const Login = ({ setAuthenticated }) => {
     }
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleLogin();
+    }
+  };
+
   return (
     <Box
       display="flex"
@@ -32,28 +43,77 @@ const Login = ({ setAuthenticated }) => {
       alignItems="center"
       justifyContent="center"
       height="100vh"
+      sx={{
+        backgroundImage: `url(${background})`, // Sử dụng biến đã import
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
     >
-      <Typography variant="h4" mb={2}>
-        Login
-      </Typography>
-      <TextField
-        label="Username"
-        variant="outlined"
-        margin="normal"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        variant="outlined"
-        margin="normal"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button variant="contained" color="primary" sx={{ mt: 2 }} onClick={handleLogin}>
-        Login
-      </Button>
+      <Box
+        bgcolor="rgba(255, 255, 255)" // Màu nền trong suốt cho form
+        p={4}
+        borderRadius={2}
+        boxShadow={3}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        width="100%"
+        maxWidth="400px"
+      >
+        <Box
+          component="img"
+          src={logo} // Đường dẫn logo
+          alt="Logo"
+          width="38%"
+          height="38%"
+          borderRadius="50%"
+          mb={2}
+        />
+        <Typography variant="h4" fontWeight="bold" color="#15508a" mb={3} textAlign="center" marginBottom={0}>
+          SỐ HÓA VĂN KIỆN
+        </Typography>
+        <TextField
+          label="Username"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <TextField
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onKeyPress={handleKeyPress}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          fullWidth
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      </Box>
     </Box>
   );
 };
